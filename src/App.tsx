4b39f271e02c4c2d54ace5c704d49ff7d9d4859b
Alex Gonzalez <7432182+luinrandir@users.noreply.github.com>
2023-05-08 10:25:23 -0700

    Cleaning up project. Reorganizing to get started.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      Reliable, efficient delivery Powered by Technology Our Artificial
+      Intelligence powered tools use millions of project data points to ensure
+      that your project is successful Supervisor Monitors activity to identify
+      project roadblocks Team Builder Scans our talent network to create the
+      optimal team for your project Karma Regularly evaluates our talent to
+      ensure quality Calculator Uses data from past projects to provide better
+      delivery estimates
+      <footer>
+        <p className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
